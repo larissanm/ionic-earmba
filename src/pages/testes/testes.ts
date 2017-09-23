f3a22b55d909a6e444958362455f2e52d5506e05
RajGod <pedro.luiz@hotmail.com.br>
2017-09-23 10:43:19 -0300

    Interface da tela Testes

--- src/pages/testes/testes.ts
+++ src/pages/testes/testes.ts
@@ -14,8 +14,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'testes.html',
 })
 export class TestesPage {
-
+  pergunta : String;
+  lblPergunta : String;
+  numQuestao : number;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.numQuestao=1;this.pergunta="Esta é djhgADFadofADPYapçiudgfA7iyfotdytdityditdiytdiytdiytdiytditydytdiytdiytdiytditydiytdtiydtdtydiytdiytdidtyidiytdytdiytdtydtydiytdiytdytiditydytdtydtydiytdityditydtydtydiytdiytdiytdiytdtiytdytdiydiytdiytdiytdytdidtydytditydrtdcgdrrdtyvbjnlkjiytrdtersdtfgylhujxcfccfcfcfcfcfcfvggtgtgybygyg5thyghtug5th6yt5tby6tt5y6gyvybyybtH7T6HUNJBYJHYHH6YJ7UJJ8IJYHBJNBHN NTGBHBHYBGBHYVYHBHYNVJBTXJHNY7NKH8UN8UJUHI9JUNHBBYYHJUN8IJ8MMIKMKUJMFWPDQ7FWDJUQfwcQUSFCOqvwcylufÇD;XFaçydçQIUFWDqvwdcVSCLYavsçfhvQCYSXSDFQDTGTGDTTDTa primeira Pergunta";
+    this.lblPergunta =this.numQuestao+" - "+this.pergunta;
   }
 
   ionViewDidLoad() {
