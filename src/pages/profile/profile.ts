bbc807247e409b910774bdb4ac4a9f9cb2c488fd
laura <moromisato.arissa@gmail.com>
2017-11-04 15:51:50 -0200

    Interface gráfica da ProfilePage incluida.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, Platform } from 'ionic-angular';
 import { ConfiguracoesPage } from '../../pages/configuracoes/configuracoes';
 import { HomePage } from '../../pages/home/home';
 
@@ -17,8 +17,22 @@ import { HomePage } from '../../pages/home/home';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  profilePicture: any = "assets/icon/profile.png"
+  character;
+  charNum : number = 0;
+
+  public patientInformation: Array<{years: number, birth: string, sex: string, phone: number, obs:string}>;
+
+  constructor(
+    public platform: Platform, public params: NavParams, public viewCtrl: ViewController) {
+
+      this.patientInformation = [
+        {years: 89,          birth: '0902020',          sex: 'Femino', phone: 19028327, obs:'Às vezes possui crises de comportamento.'},
+      ];
+
+    }
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
